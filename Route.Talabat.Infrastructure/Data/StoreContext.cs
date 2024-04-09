f81f6b98b242addd9bb26f7978d4864a78ae96f2
NermeenSameh <nermeensameh1998@gmail.com>
2024-04-09 22:05:43 +0200

    DbContext, ProductConfigurations, Migrations

---
+++ Route.Talabat.Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Route.Talabat.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Infrastructure.Data
+{
+	public class StoreContext : DbContext
+	{
+		public StoreContext(DbContextOptions<StoreContext> options) : base(options) { }
+
+
+
+		/// protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		/// 	=> optionsBuilder.UseSqlServer("connectionString");
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		}
+
+
+		public DbSet<Product> Products { get; set; }
+
+		public DbSet<ProductBrand> Brands { get; set; }
+
+		public DbSet<ProductCategory> Categories { get; set; }
+
+	}
+}
