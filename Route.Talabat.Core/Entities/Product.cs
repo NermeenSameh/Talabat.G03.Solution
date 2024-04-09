3a627739f484b9b392c6a8c84baf95d0479885df
NermeenSameh <nermeensameh1998@gmail.com>
2024-04-09 20:56:15 +0200

    Product Module - Entities

---
+++ Route.Talabat.Core/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Core.Entities
+{
+	public class Product : BaseEntity
+	{
+		public string Name { get; set; } = null!;
+		public string Description { get; set; } = null!;
+
+		public string PictureUrl { get; set; } = null!;
+
+		public decimal Price { get; set; }
+
+		public int BrandId { get; set; } // Foreign Key Column => ProductBrand
+
+		public int CategoryId { get; set; }
+
+		public ProductBrand Brands { get; set; }  // Navigational Property [ONE]
+
+		public ProductCategory Category { get; set; }   // Navigational Property [ONE]
+
+	}
+}
---
+++ Route.Talabat.Core/Entities/ProductBrand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Core.Entities
+{
+	public class ProductBrand : BaseEntity
+	{
+
+        public string Name { get; set; }
+
+		//public ICollection<Product> Products { get; set; } = new HashSet<Product>();
+  
+	}
+}
