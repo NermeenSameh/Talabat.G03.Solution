1a9e3558b2b7c0b5ad048b33ac903cb9067cc71e
NermeenSameh <nermeensameh1998@gmail.com>
2024-04-10 04:35:43 +0200

    Products Controller

---
+++ Talabat.APIs/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Route.Talabat.Core.Entities;
+using Route.Talabat.Core.Repositories.Contract;
+
+namespace Talabat.APIs.Controllers
+{
+	
+	public class ProductController : BaseApiController
+	{
+		private readonly IGenericRepository<Product> _productRepo;
+
+		public ProductController(IGenericRepository<Product> productRepo )
+        {
+			_productRepo = productRepo;
+		}
+
+    }
+}
