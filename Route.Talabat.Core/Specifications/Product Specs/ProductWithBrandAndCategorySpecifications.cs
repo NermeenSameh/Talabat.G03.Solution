2fb2e59b5b2f0ad932a12007ff6c5b58c891f195
NermeenSameh <nermeensameh1998@gmail.com>
2024-04-16 06:51:57 +0200

    Refactor GetProductById Using Specification DP

--- Route.Talabat.Core/Specifications/Product Specs/ProductWithBrandAndCategorySpecifications.cs
+++ Route.Talabat.Core/Specifications/Product Specs/ProductWithBrandAndCategorySpecifications.cs
@@ -10,12 +10,24 @@ namespace Route.Talabat.Core.Specifications.Product_Specs
 	public class ProductWithBrandAndCategorySpecifications : BaseSpecifications<Product>
 	{
         // This Constructor will be Used for Creating an Object , That will be Used to Get All Product
-        public ProductWithBrandAndCategorySpecifications():base()
-        {
-            Includes.Add(P => P.Brands);
-            Includes.Add(P => P.Category);
+        public ProductWithBrandAndCategorySpecifications()
+			:base()
+		{
+			AddIncludes();
 
-        }
+		}
+
+		// This Constructor will be Used for Creating an object, that will be used to Get A specific product with id
+		public ProductWithBrandAndCategorySpecifications(int id)
+			:base(P => P.Id == id)
+        {
+			AddIncludes();
+		}
+		private void AddIncludes()
+		{
+			base.Includes.Add(P => P.Brands);
+			base.Includes.Add(P => P.Category);
+		}
 
     }
 }
