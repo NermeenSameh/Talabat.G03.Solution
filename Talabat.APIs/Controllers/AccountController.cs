6c0be4c7fb6cbe1cc3c2128cb3ca4c787fcf4c4f
NermeenSameh <nermeensameh1998@gmail.com>
2024-04-28 00:59:41 +0300

    Register Endpoint

--- Talabat.APIs/Controllers/AccountController.cs
+++ Talabat.APIs/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32;
 using Route.Talabat.Core.Entities.Identity;
 using Talabat.APIs.DTOs;
 using Talabat.APIs.Errors;
@@ -22,7 +23,7 @@ namespace Talabat.APIs.Controllers
 		}
 
 
-		[HttpPost("login")]
+		[HttpPost("login")]  // Post: /api/Account/login
 
 		public async Task<ActionResult<UserDto>> Login(LoginDto model)
 		{
@@ -32,7 +33,7 @@ namespace Talabat.APIs.Controllers
 
 			var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
 
-			if (result.Succeeded) return Unauthorized(new ApiResponse(401, "Invalid login"));
+			if (!result.Succeeded) return Unauthorized(new ApiResponse(401, "Invalid login"));
 
 			return Ok(new UserDto()
 			{
@@ -42,6 +43,29 @@ namespace Talabat.APIs.Controllers
 			});
 		}
 
+		[HttpPost("register")] // Post : /api/Account/register
+
+		public async Task<ActionResult<UserDto>> Register(RegisterDto model)
+		{
+			var user = new ApplicationUser()
+			{
+				DisplayName = model.DisplayName,
+				Email = model.Email,
+				UserName = model.Email.Split("@")[0],
+				PhoneNumber = model.Phone
+			};
+
+			var result = await _userManager.CreateAsync(user, model.Password);
+
+			if (!result.Succeeded) return BadRequest(new ApiValidationErrorResponse() { Errors = result.Errors.Select(E => E.Description) });
+
+			return Ok(new UserDto()
+			{
+				DisplayName = user.DisplayName,
+				Email = user.Email,
+				Token = "This will be Token"
+			});
+		}
 
 	}
 }
---
+++ Talabat.APIs/DTOs/RegisterDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Talabat.APIs.DTOs
+{
+	public class RegisterDto
+	{
+		[Required]
+		public string DisplayName { get; set; } = null!;
+
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; } = null!;
+
+		[Required]
+		public string Phone { get; set; } = null!;
+
+		[Required]
+		//[RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$"
+		//	,ErrorMessage = "Password 1 Uppercase, 1Lowercase, 1 number, 1 non alphanumeric and at least 6 characters")]
+
+		public string Password { get; set; } = null!;
+
+    }
+}
