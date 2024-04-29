8b12550f3babda98d1c7476bb1f365e573ee686a
NermeenSameh <nermeensameh1998@gmail.com>
2024-04-29 14:04:59 +0300

    Auth Service - JWT Token Creation

---
+++ Route.Talabat.Core/Services.Contract/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Route.Talabat.Core.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Core.Services.Contract
+{
+	public interface IAuthService
+	{
+
+		Task<string> CreateTokenAsync(ApplicationUser user , UserManager<ApplicationUser> userManager);
+	}
+}
