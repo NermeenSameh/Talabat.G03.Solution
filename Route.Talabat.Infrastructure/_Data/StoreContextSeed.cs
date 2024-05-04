0ec017771a56b2cca3332b02befcdce163f7a2c1
NermeenSameh <nermeensameh1998@gmail.com>
2024-05-05 01:54:44 +0300

    Delivery Methods - Data Seeding

--- Route.Talabat.Infrastructure/_Data/StoreContextSeed.cs
+++ Route.Talabat.Infrastructure/_Data/StoreContextSeed.cs
@@ -1,4 +1,5 @@
-﻿using Route.Talabat.Core.Entities.Product;
+﻿using Route.Talabat.Core.Entities.Order_Aggregate;
+using Route.Talabat.Core.Entities.Product;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,7 @@ namespace Route.Talabat.Infrastructure.Data
 		{
 			if (_dbContext.Brands.Count() == 0)
 			{
-				var brandsData = File.ReadAllText("../Route.Talabat.Infrastructure/Data/DataSeed/brands.json");
+				var brandsData = File.ReadAllText("../Route.Talabat.Infrastructure/_Data/DataSeed/brands.json");
 				var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
 
 				if (brands?.Count > 0)
@@ -32,7 +33,7 @@ namespace Route.Talabat.Infrastructure.Data
 
 			if (_dbContext.Categories.Count() == 0)
 			{
-				var categoryData = File.ReadAllText("../Route.Talabat.Infrastructure/Data/DataSeed/categories.json");
+				var categoryData = File.ReadAllText("../Route.Talabat.Infrastructure/_Data/DataSeed/categories.json");
 				var categories = JsonSerializer.Deserialize<List<ProductCategory>>(categoryData);
 
 				if (categories?.Count > 0)
@@ -49,7 +50,7 @@ namespace Route.Talabat.Infrastructure.Data
 
 			if (_dbContext.Products.Count() == 0)
 			{
-				var productData = File.ReadAllText("../Route.Talabat.Infrastructure/Data/DataSeed/products.json");
+				var productData = File.ReadAllText("../Route.Talabat.Infrastructure/_Data/DataSeed/products.json");
 				var products = JsonSerializer.Deserialize<List<Product>>(productData);
 
 				if (products?.Count > 0)
@@ -58,6 +59,25 @@ namespace Route.Talabat.Infrastructure.Data
 					foreach (var product in products)
 					{
 						_dbContext.Set<Product>().Add(product);
+					}
+					await _dbContext.SaveChangesAsync();
+
+				}
+			}
+
+
+
+			if (_dbContext.DeliveryMethods.Count() == 0)
+			{
+				var deliveryMethodData = File.ReadAllText("../Route.Talabat.Infrastructure/_Data/DataSeed/delivery.json");
+				var deliveryMethods = JsonSerializer.Deserialize<List<DeliveryMethod>>(deliveryMethodData);
+
+				if (deliveryMethods?.Count > 0)
+				{
+
+					foreach (var deliveryMethod in deliveryMethods)
+					{
+						_dbContext.Set<DeliveryMethod>().Add(deliveryMethod);
 					}
 					await _dbContext.SaveChangesAsync();
 
