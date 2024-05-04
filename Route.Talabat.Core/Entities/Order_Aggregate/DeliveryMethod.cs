5f9685afa4fa622ec138caf222c3f6dd8db654c9
NermeenSameh <nermeensameh1998@gmail.com>
2024-05-05 00:07:02 +0300

    Order Module - Entities

---
+++ Route.Talabat.Core/Entities/Order_Aggregate/DeliveryMethod.cs
@@ -0,0 +1,24 @@
+﻿using Route.Talabat.Core.Entities.Baskets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Core.Entities.Order_Aggregate
+{
+	public class DeliveryMethod : BaseEntity
+	{
+
+		public string ShortName { get; set; } = null!;
+
+		public string Description { get; set; } = null!;
+
+		public decimal Cost { get; set; }
+
+		public string DeliveryTime { get; set; } = null!;
+	
+	//	public ICollection<Order> Order { get; set; } = new HashSet<Order>();	
+	
+	}
+}
---
+++ Route.Talabat.Core/Entities/Order_Aggregate/Order.cs
@@ -0,0 +1,35 @@
+﻿using Route.Talabat.Core.Entities.Baskets;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Core.Entities.Order_Aggregate
+{
+	public class Order : BaseEntity
+	{
+		public string BuyerEmail { get; set; } = null!;
+
+		public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.UtcNow;
+
+		public OrderStatus Status { get; set; } = OrderStatus.Pending;
+
+		public Address ShappingAddress { get; set; } = null!;
+
+		public int DeliveryMethodId { get; set; } // FOreign Key 
+		public DeliveryMethod DeliveryMethod { get; set; } = null!;  // Navigational Property  [ONE]
+
+		public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();  // Navigational Property [MANY]
+
+		public decimal Subtotal { get; set; }
+
+		//[NotMapped]
+		//public decimal Total => Subtotal + DeliveryMethod.Cost;
+
+        public decimal GetTotal() => Subtotal + DeliveryMethod.Cost;
+
+		public string PaymentIntenId { get; set; } = string.Empty;
+    }
+}
