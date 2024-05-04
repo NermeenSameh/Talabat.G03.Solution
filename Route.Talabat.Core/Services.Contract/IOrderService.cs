31b9ff45d116608fa9a5c89171f76ad637d43f44
NermeenSameh <nermeensameh1998@gmail.com>
2024-05-05 02:10:20 +0300

    Order Service

---
+++ Route.Talabat.Core/Services.Contract/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using Route.Talabat.Core.Entities.Order_Aggregate;
+
+
+namespace Route.Talabat.Core.Services.Contract
+{
+	public interface IOrderService
+	{
+		Task<Order> CreateOrderAsync(string buyerEmail,string basketId,string deliveryMethodId , Address shippingAddress);
+
+		Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail);
+
+		Task<Order> GetOrderByIdForUserAsync(string buyerEmail , int orderId);
+
+		Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync();
+
+
+	}
+}
---
+++ Route.Talabat.Services/OrderService/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using Route.Talabat.Core.Entities.Order_Aggregate;
+using Route.Talabat.Core.Services.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Service.OrderService
+{
+	public class OrderService : IOrderService
+	{
+		public Task<Order> CreateOrderAsync(string buyerEmail, string basketId, string deliveryMethodId, Address shippingAddress)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Order> GetOrderByIdForUserAsync(string buyerEmail, int orderId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
