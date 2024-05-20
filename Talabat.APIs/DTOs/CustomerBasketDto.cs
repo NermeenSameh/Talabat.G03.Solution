7ce0a9a039f15a1d97456b9ab9ebdfb561bb95fe
NermeenSameh <nermeensameh1998@gmail.com>
2024-05-20 04:38:22 +0300

    Create Order with PaymentIntent - Debug

--- Talabat.APIs/DTOs/CustomerBasketDto.cs
+++ Talabat.APIs/DTOs/CustomerBasketDto.cs
@@ -9,5 +9,14 @@ namespace Talabat.APIs.DTOs
 		public string Id { get; set; } = null!;
 
 		public List<BasketItemDto> Items { get; set; }
+
+		public string? PaymentIntentId { get; set; }
+
+		public string? ClientSecret { get; set; }
+
+		public int? DeliveryMethodId { get; set; }
+
+		public decimal ShippingPrice { get; set; }
+
 	}
 }
