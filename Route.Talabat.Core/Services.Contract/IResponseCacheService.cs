a49f0aa08c49ba46edbbd52c5c56e0c8bcdf1d73
NermeenSameh <nermeensameh1998@gmail.com>
2024-05-30 17:25:19 +0300

    Caching Service

---
+++ Route.Talabat.Core/Services.Contract/IResponseCacheService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.Talabat.Core.Services.Contract
+{
+	public interface IResponseCacheService
+	{
+		Task CacheResponseAsync(string Key, object Response, TimeSpan timeToLive);
+
+		Task<string?> GetCachedResponseAsync(string Key);
+
+	}
+}
